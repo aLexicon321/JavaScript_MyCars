6fc0a9765b4a9a82bb0d9252da2d07331c3467ca
aLexi <91291227+aLexicon321@users.noreply.github.com>
2021-10-14 09:52:08 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,12 +1,17 @@
-let li = "";
-
 const cars = [        
     { brand : "BMW",    model : "M2"    },
     { brand : "Volvo",  model : "XC60"  },
     { brand : "Yugo",   model : "Coral" }
 ];
+const new_Honda = {
+    color: "red", wheels: 4,
+    brand: "Honda", model: "Miata",
+    engine: { cylinders: 4, size: 2.2 }
+};
+cars.push(new_Honda);
 
 function loadMyCars(){
+    li = "";
     cars.forEach(car => li += `<li>${car.brand} ${car.model}</li>`);
     document.getElementById('carList').innerHTML = li;
 }
