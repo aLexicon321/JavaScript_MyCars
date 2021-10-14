b3f66970c08db2bfbf761cb1f2d43e828dfc07c2
aLexi <91291227+aLexicon321@users.noreply.github.com>
2021-10-14 11:03:16 +0200

    updates, added input fields for brand and model

---
+++ cars.js
@@ -0,0 +1,12 @@
+const cars = [        
+    { brand : "BMW",    model : "M2"    },
+    { brand : "Volvo",  model : "XC60"  },
+    { brand : "Yugo",   model : "Coral" }
+];
+
+const new_Honda = {
+    color: "red", wheels: 4,
+    brand: "Honda", model: "Miata",
+    engine: { cylinders: 4, size: 2.2 }
+};
+cars.push(new_Honda);
--- script.js
+++ script.js
@@ -1,17 +1,28 @@
-const cars = [        
-    { brand : "BMW",    model : "M2"    },
-    { brand : "Volvo",  model : "XC60"  },
-    { brand : "Yugo",   model : "Coral" }
-];
-const new_Honda = {
-    color: "red", wheels: 4,
-    brand: "Honda", model: "Miata",
-    engine: { cylinders: 4, size: 2.2 }
-};
-cars.push(new_Honda);
+const form = document.querySelector('#newCarField');
+const input = form.querySelectorAll('input');
 
 function loadMyCars(){
     li = "";
     cars.forEach(car => li += `<li>${car.brand} ${car.model}</li>`);
     document.getElementById('carList').innerHTML = li;
+}
+
+function createCar(){
+    let brand = input[0].value;
+    let model = input[1].value;
+    const newCar = { brand, model };
+
+    if(brand !== "" && model !== ""){
+        cars.push(newCar);
+        clearFieldsAndRefresh();
+    }else{
+        console.warn("Err: check input", newCar);
+        alert("Check input fields...");
+    }
+}
+
+function clearFieldsAndRefresh(){
+    input[0].value = "";
+    input[1].value = "";
+    loadMyCars();
 }
