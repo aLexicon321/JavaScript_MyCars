9aa65e438bf143ae1e0b545859bab3b2c066c328
aLexi <91291227+aLexicon321@users.noreply.github.com>
2021-10-14 23:14:58 +0200

    Style and CRUD

--- cars.js
+++ cars.js
@@ -1,12 +1,12 @@
 const cars = [        
-    { brand : "BMW",    model : "M2"    },
-    { brand : "Volvo",  model : "XC60"  },
-    { brand : "Yugo",   model : "Coral" }
+    { brand : "BMW",    model : "M2",    year : "2020"  },
+    { brand : "Volvo",  model : "XC60",  year : "2019"  },
+    { brand : "Yugo",   model : "Coral", year : "1996"  }
 ];
 
 const new_Honda = {
     color: "red", wheels: 4,
-    brand: "Honda", model: "Miata",
-    engine: { cylinders: 4, size: 2.2 }
+    engine: { cylinders: 4, size: 2.2 },
+    brand: "Honda", model: "Miata", year : "2004"
 };
 cars.push(new_Honda);
--- script.js
+++ script.js
@@ -1,16 +1,18 @@
 const form = document.querySelector('#newCarField');
 const input = form.querySelectorAll('input');
+const 
+    field_brand = input[0],
+    field_model = input[1],
+    field_year = input[2];
 
-function loadMyCars(){
-    li = "";
-    cars.forEach(car => li += `<li>${car.brand} ${car.model}</li>`);
-    document.getElementById('carList').innerHTML = li;
-}
+let currentId = null;
 
+// CREATE
 function createCar(){
-    let brand = input[0].value;
-    let model = input[1].value;
-    const newCar = { brand, model };
+    let brand = field_brand.value;
+    let model = field_model.value;
+    let year = field_year.value;
+    const newCar = { brand, model, year };
 
     if(brand !== "" && model !== ""){
         cars.push(newCar);
@@ -21,8 +23,67 @@ function createCar(){
     }
 }
 
+// READ
+function readCars(){
+    let tr = "";
+
+    console.log('ReadCars:', cars);
+    cars.forEach((car, index) => 
+        tr += 
+        `<tr id="carNr${index}">
+            <td> ${car.brand} </td>
+            <td> ${car.model} </td>
+            <td> ${car.year || ""} </td>
+            <td>
+                <input type="button" value="X" onclick="deleteCar(${index})">
+                <input type="button" value="EDIT" onclick="editCar(${index})">
+            </td>
+        </tr>`
+    );
+    document.getElementById('carTable').innerHTML = tr;
+}
+
+// EDIT
+function updateCar(){
+
+    console.log("[UPDATE] ID:",{ id : currentId });
+    cars[currentId] = {
+        brand : field_brand.value,
+        model : field_model.value,
+        year : field_year.value
+    }
+    readCars();
+    clearFieldsAndRefresh();
+
+    document.querySelector('#createBtn').hidden = false;
+    document.querySelector('#updateBtn').hidden = true;
+}
+
+// UPDATE
+function editCar(id){
+
+    currentId = id;
+
+    console.log("[EDIT] ID:" + id);
+    const car = cars[id];
+    field_brand.value = car.brand;
+    field_model.value = car.model;
+    field_year.value = car.year || "";
+
+    document.querySelector('#createBtn').hidden = true;
+    document.querySelector('#updateBtn').hidden = false;
+}
+
+// DELETE
+function deleteCar(id){
+    console.log("[DELETE] ID:", { id, car : cars[id] });
+    cars.splice(id,1);
+    readCars();
+}
+
 function clearFieldsAndRefresh(){
     input[0].value = "";
     input[1].value = "";
-    loadMyCars();
+    input[2].value = "";
+    readCars();
 }
